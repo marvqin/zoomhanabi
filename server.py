8ca1284be1dfb83cee2172b952d6b3e5cd12e3a5
akwiat <na>
2020-04-18 13:29:24 -0600

    setting up events

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import eventlet
 import socketio
+from Room import Room
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
@@ -7,17 +8,18 @@ app = socketio.WSGIApp(sio, static_files={
     '/modules': 'modules'
 })
 
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
-
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
-
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
+# @sio.event
+# def connect(sid, environ):
+#     print('connect ', sid)
+#
+# @sio.event
+# def my_message(sid, data):
+#     print('message ', data)
+#
+# @sio.event
+# def disconnect(sid):
+#     print('disconnect ', sid)
 
 if __name__ == '__main__':
+    r = Room(sio)
     eventlet.wsgi.server(eventlet.listen(('', 3000)), app)
